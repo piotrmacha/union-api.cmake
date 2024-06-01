537af55fcda7d199fa0ab15dce07774d164f0f2a
Piotr Macha <me@piotrmacha.pl>
2024-06-01 08:35:57 +0200

    Initial commit

---
+++ cmake/union-api/union-api.cmake
@@ -0,0 +1,17 @@
+add_library(${UNION_API_BUILD_TARGET} ${UNION_API_BUILD_TYPE}
+        ${UNION_API_SRC_DIR}/union-api/union-api.cpp
+        ${UNION_API_SRC_DIR}/union-api/Union/Memory.cpp)
+
+target_include_directories(${UNION_API_BUILD_TARGET}
+        PUBLIC $<BUILD_INTERFACE:${UNION_API_SRC_DIR}/union-api>
+        PUBLIC $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
+
+target_link_directories(${UNION_API_BUILD_TARGET}
+        PUBLIC $<BUILD_INTERFACE:${UNION_API_SRC_DIR}/union-api>
+        PUBLIC $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
+
+target_compile_definitions(${UNION_API_BUILD_TARGET}
+        PUBLIC WIN32 _CONSOLE
+        PUBLIC $<IF:$<STREQUAL:${UNION_API_BUILD_TYPE},SHARED>,_UNION_API_DLL,_UNION_API_LIB>
+        PUBLIC $<$<CONFIG:Debug>:_DEBUG:NDEBUG>
+        PRIVATE _UNION_API_BUILD)
